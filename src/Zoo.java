6270e7615e3979ec3f6e4bf6228f76cc3261f1c9
Nico Tolvanen <nicotolvanen@gmail.com>
2023-02-06 20:39:53 +0200

    did chan´ge

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,10 +1,24 @@
 import java.util.ArrayList;
 
 public class Zoo {
-    ArrayList lista = new ArrayList<>();
-    
-    
-    Zoo(ArrayList lista){
+   
+    private ArrayList<Animal> lista = new ArrayList<Animal>();
+
+
+
+    public Zoo() {}
+
+    public void addAnimal(Animal eläin) {
+        this.lista.add(eläin);
+    }
+    public void listAnimal(){
+        for (Animal eläin : this.lista) {
+          System.out.println(eläin);  
+        }
+    }
+    public void runAnimals(){
         
     }
+
+ 
 }
